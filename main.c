8ada6d35249717cc0c2bb07b0f790bc130453124
Heleno Campos <camposheleno0@gmail.com>
2019-03-31 18:48:04 -0300

    First constructive heuristic (nearest neighbor) implemented

--- main.c
+++ main.c
@@ -19,8 +19,7 @@
  * 
  */
 
-#undef M_PI
-#define M_PI 3.14159265358979323846264
+#define BIG_DISTANCE 999999
 
 struct TSPLibData {
     int citiesAmount;
@@ -83,13 +82,13 @@ void allocateTSPInstanceEuclidian2D(struct TSPLibData *data, struct TSPInstance 
         instance->graphMatrix[i] = malloc(instance->citiesAmount * sizeof (double));
     }
 
+    //    for (int i = 0; i < instance->citiesAmount; i++) {
+    //        free(instance->graphMatrix[i]);
+    //    }
+    //    free(instance->graphMatrix);
     for (int i = 0; i < instance->citiesAmount; i++) {
-        free(instance->graphMatrix[i]);
-    }
-    free(instance->graphMatrix);
-    for(int i=0; i< instance->citiesAmount; i++){
-        for(int j=0; j< instance->citiesAmount; j++){
-            instance->graphMatrix[i][j]=getEuclidianDistance(i,j,data);
+        for (int j = 0; j < instance->citiesAmount; j++) {
+            instance->graphMatrix[i][j] = getEuclidianDistance(i, j, data);
         }
     }
 
@@ -113,10 +112,15 @@ void parseTSPLibFileEuclidian2D(char *filePath, struct TSPLibData *data, struct 
                     if (vertexAmount == 0) {
                         char delim[] = " ";
                         char *ptr = strtok(line, delim);
-                        if (strcmp(ptr, "DIMENSION:") == 0) {
+                        
+                        if (strcmp(ptr, "DIMENSION") == 0) {
                             ptr = strtok(NULL, delim);
+                            if(strcmp(ptr, ":")==0){
+                                ptr = strtok(NULL, delim);
+                            }
                             vertexAmount = atoi(ptr);
                             data->citiesAmount = vertexAmount;
+//                            printf("%d",data->citiesAmount);
                             data->x = malloc(vertexAmount * sizeof *data->x);
                             data->y = malloc(vertexAmount * sizeof *data->y);
                         }
@@ -132,6 +136,7 @@ void parseTSPLibFileEuclidian2D(char *filePath, struct TSPLibData *data, struct 
                         float x = strtod(ptr, NULL);
                         ptr = strtok(NULL, delim);
                         float y = strtod(ptr, NULL);
+//                        printf("\nvertexCounter: %d, cities: %d", vertexCounter, data->citiesAmount);
                         data->x[vertexCounter] = x;
                         data->y[vertexCounter] = y;
                         vertexCounter++;
@@ -156,27 +161,84 @@ void printTSPLibData(struct TSPLibData *data) {
 }
 
 void printInstanceData(struct TSPInstance *instance) {
-        for (int i = 1; i <= instance->citiesAmount; i++) {
-            for (int j = 1; j <= instance->citiesAmount; j++) {
-                if (j == instance->citiesAmount) {
-                    printf("\n");
-                }
-                printf("\nmatrix[%d][%d]=%.2f", i, j, instance->graphMatrix[i][j]);
-    
+    for (int i = 1; i <= instance->citiesAmount; i++) {
+        for (int j = 1; j <= instance->citiesAmount; j++) {
+            if (j == instance->citiesAmount) {
+                printf("\n");
             }
+            printf("\nmatrix[%d][%d]=%.2f", i, j, instance->graphMatrix[i][j]);
+
         }
+    }
+}
+
+void printRoute(int route[], int routeSize, double length) {
+    printf("Route: ");
+    for (int i = 0; i < routeSize; i++) {
+        printf(" %d ", route[i]);
+    }
+    printf("\nTotal length: %f", length);
+}
+
+int getNearestVertexNotVisited(double distances[], int vertexAmount, int origin, int visitedVertexes[]) {
+    double minDistance = BIG_DISTANCE;
+    int minDistanceVertex = -1;
+    for (int j = 0; j < vertexAmount; j++) {
+//        printf("\n %d to %d = %f. visited: %d", origin, j, distances[j], visitedVertexes[j]);
+        if (distances[j] < minDistance && visitedVertexes[j] == 0 && origin != j) {
+            minDistance = distances[j];
+            minDistanceVertex = j;
+        }
+    }
+    return minDistanceVertex;
+}
+
+void initializeArray(int array[], int size, int value) {
+    for (int i = 0; i < size; i++) {
+        array[i] = value;
+    }
+}
+
+void symetricGreedyTSP(int startingNode, struct TSPInstance *instance) { //using a greedy approach 
+
+    if (startingNode < instance->citiesAmount) {
+        int visitedVertexes[instance->citiesAmount]; //if value is 1 then the vertex was already visited
+        initializeArray(visitedVertexes, instance->citiesAmount, 0);
+        double totalDistance = 0;
+        int routeOrder[instance->citiesAmount + 1];
+        routeOrder[0] = startingNode;
+        visitedVertexes[startingNode] = 1;
+        int lastVisited = startingNode;
+        for (int visitNumber = 0; visitNumber < instance->citiesAmount; visitNumber++) {
+            //            //find the edge with the minimum distance among the not visited vertexes
+            int nearestNotVisited = getNearestVertexNotVisited(instance->graphMatrix[visitNumber],
+                    instance->citiesAmount, visitNumber, visitedVertexes);
+
+            visitedVertexes[nearestNotVisited] = 1;
+            totalDistance += instance->graphMatrix[lastVisited][nearestNotVisited];
+            routeOrder[visitNumber + 1] = nearestNotVisited;
+//            printf("\n nearest: %d visitnumber: %d", nearestNotVisited, visitNumber);
+
+            lastVisited = nearestNotVisited;
+        }
+        printRoute(routeOrder, instance->citiesAmount, totalDistance);
+    }
 }
 
 int main(int argc, char** argv) {
 
     struct TSPLibData tspLibData;
     struct TSPInstance tspInstance;
+
     //printf("Initializing\n\n");
     //printf("%d", isValidEuclidian2Dfile("instances/a280.tsp.txt"));
-    parseTSPLibFileEuclidian2D("instances/a280.tsp.txt", &tspLibData, &tspInstance);
-    //printTSPLibData(&tspLibData);
+//    parseTSPLibFileEuclidian2D("instances/a280.tsp.txt", &tspLibData, &tspInstance);
+    parseTSPLibFileEuclidian2D("instances/d18512.tsp", &tspLibData, &tspInstance);
+//    printTSPLibData(&tspLibData);
+
     allocateTSPInstanceEuclidian2D(&tspLibData, &tspInstance);
-    printInstanceData(&tspInstance);
+    //    printInstanceData(&tspInstance);
+    symetricGreedyTSP(1, &tspInstance);
     return (EXIT_SUCCESS);
 }
 
