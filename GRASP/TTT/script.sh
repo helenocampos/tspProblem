984187f7abda7e1ce38b5a01e4660052d6197870
Heleno Campos <camposheleno0@gmail.com>
2019-05-24 13:14:25 -0300

    Update target values for experiment 3

--- GRASP/TTT/script.sh
+++ GRASP/TTT/script.sh
@@ -1,17 +1,17 @@
 !#!/bin/bash
 dataPoints=10;
-eil51=433;
-berlin52=7842;
-pr144=58862;
-pr299=49613;
-u574=38916;
-rat783=9250;
-vm1084=254490;
-pcb1173=60421;
-d1291=52963;
-rl1304=272037;
-vm1748=353289;
-u1817=60433;
+eil51=448;
+berlin52=7920;
+pr144=61464;
+pr299=50601;
+u574=38751;
+rat783=9247;
+vm1084=251262;
+pcb1173=59737;
+d1291=53342;
+rl1304=265596;
+vm1748=353384;
+u1817=60062;
 
 ./tspProblem GRASP_TTT_eil51 RDSNN first $dataPoints file instances/small/eil51.tsp 0 ql 3 $eil51 25;
 ./tspProblem GRASP_TTT_berlin52 RDSNN first $dataPoints file instances/small/berlin52.tsp 0 ql 3 $berlin52 25;
