c5c0fb1105240c460b1a35d537d5254a3684970a
Heleno Campos <camposheleno0@gmail.com>
2019-06-06 12:50:02 -0300

    add new experiment scripts for path relinking

---
+++ path_relinking/third_instance/experimentScript.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+./tspProblem TTT_vm1748_GRASP RDSNN first 200 file instances/medium/vm1748.tsp 0 ql 3 387533 100 25;
+./tspProblem TTT_vm1748_GRASP_PR RDSNN first 200 file instances/medium/vm1748.tsp 0 ql 4 387533 100 25;
+./tspProblem TTT_vm1748_GRASP RDSNN first 200 file instances/medium/vm1748.tsp 0 ql 3 387533 100 25;
+./tspProblem TTT_vm1748_GRASP_PR RDSNN first 200 file instances/medium/vm1748.tsp 0 ql 4 387533 100 25;
