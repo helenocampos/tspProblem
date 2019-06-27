d0f4ead59e63c60cc314e9085800a18b39c0e01b
Heleno Campos <camposheleno0@gmail.com>
2019-06-27 12:57:44 -0300

    add extra files for further experiment path relinking

---
+++ path_relinking/new_execution/further/experimentScript.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+./tspProblem pcb1173_GRASP_PR RDSNN first 1 file instances/medium/pcb1173.tsp 0 ql 4 1 1000 25;
+./tspProblem u1817_GRASP_PR RDSNN first 1 file instances/medium/u1817.tsp 0 ql 4 1 1000 25;
